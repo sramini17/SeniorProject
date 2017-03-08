3a9d47eb243ccaf1f52d03070ef4046add9064cf
sramini <sramini@calpoly.edu>
2017-03-08 14:46:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/seniorproject/Display.java
@@ -0,0 +1,15 @@
+package com.example.seniorproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sramini on 2/12/17.
+ */
+
+public class Display extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+}
