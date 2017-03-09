e9505d1f32dc20fb760b3827bf2acce06d59e415
sramini <sramini@calpoly.edu>
2017-03-08 16:58:24 -0800

    updated Files

--- app/src/main/java/com/example/seniorproject/MainActivity.java
+++ app/src/main/java/com/example/seniorproject/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   /* public void onButtonClick(View v) {
-        if(v.getId() == R.id.signInButton) {
-            Intent i = new Intent(this, Display.class);
-            startActivity(i);
-        }*/
+    public void creatAccount(View v) {
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
+    }
+
+    public void signIn(View v) {
+        Intent i = new Intent(this, Display.class);
+        startActivity(i);
+    }
+
 
 }
